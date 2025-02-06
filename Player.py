748ca14e53b7f89fd646acf4af5e4602cf18f2db
cris <142470805+CrisCariqueo@users.noreply.github.com>
2025-02-06 00:05:42 -0300

    Movement and physiscs added to player
    Player.py
    + File name changed to player.py
    + Deleted old methods
    + Added new methods for movement and physics
    
    movementTest.py
    + Added new file for testing movement and physics
    
    resources/sprite/azuball_spritesheet.png
    + Corrected vertical position for chiyo_n and chiyo_ball_n sprites
    
    resources/sprite/azuball_spritesheet.json
    + Changed "smartupdate" thingy to something not important

--- Player.py
+++ Player.py
@@ -1,33 +1,54 @@
 import pygame
+from resources.sprite.spritesheet import Spritesheet
 
-class player:
-    def __init__(self, sprite: str, initialPos: tuple, speed: list[int], deathSound: str):
-        self.sprite = pygame.image.load(sprite)
-        self.box    = self.sprite.get_rect()
-        self.box.move_ip(initialPos[0], initialPos[1])
-        self.speed = speed
-        self.deathSound = pygame.mixer.Sound(deathSound)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = Spritesheet("resources/sprite/azuball_spritesheet.png").parse_sprite("osaka_0.png")
+        self.rect = self.image.get_rect()
+        self.LEFT_KEY, self.RIGHT_KEY = False, False
+        self.is_jumping, self.on_ground = False, False
+        self.gravity, self.friction = 1.5, -0.07
+        self.position, self.velocity = pygame.math.Vector2(0, 0), pygame.math.Vector2(0, 0)
+        self.acceleration = pygame.math.Vector2(0, self.gravity)
 
-    def handle_keys(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.box.move_ip(-6,0)
-        if keys[pygame.K_RIGHT]:
-            self.box.move_ip(6,0)
-        if keys[pygame.K_UP]:
-            self.box.move_ip(0,-6)
-        if keys[pygame.K_DOWN]:
-            self.box.move_ip(0,6)
+    def draw(self, display):
+        display.blit(self.image, self.rect)
 
-    def die(self):
-        # self.deathSound.play()
-        pass
+    def update(self, dt):
+        self.horizontal_movement(dt)
+        self.vertical_movement(dt)
 
-    def render(self, window: pygame.Surface):
-        window.blit(self.sprite, self.box)
+    def horizontal_movement(self, dt):
+        self.acceleration.x = 0
+        if self.LEFT_KEY:
+            self.acceleration.x -= 1
+        elif self.RIGHT_KEY:
+            self.acceleration.x += 1
+        self.acceleration.x += self.velocity.x * self.friction
+        self.velocity.x += self.acceleration.x * dt
+        self.limit_velocity(10)
+        self.position.x += self.velocity.x * dt + (self.acceleration.x * 0.5) * (dt * dt)
+        self.rect.x = self.position.x
 
-    def pos_render(self, window: pygame.Surface):
-        x, y = self.box.x, self.box.y
-        font = pygame.font.Font(None, 36)
-        text = font.render(f"coords: x:{x}, y:{y}", 1, (255, 255, 255))
-        window.blit(text, (550, 10))
+    def vertical_movement(self, dt):
+        self.velocity.y += self.acceleration.y * dt
+        if self.velocity.y > 10:
+            self.velocity.y = 10
+        self.position.y += self.velocity.y * dt + (self.acceleration.y * 0.5) * (dt * dt)
+        if self.position.y > 600:
+            self.position.y = 600
+            self.velocity.y = 0
+            self.on_ground = True
+        self.rect.bottom = self.position.y
+
+    def limit_velocity(self, max_vel):
+        self.velocity.x = min(max_vel, max(self.velocity.x, -max_vel))
+        if abs(self.velocity.x) < 0.2:
+            self.velocity.x = 0
+
+    def jump(self):
+        if self.on_ground:
+            self.velocity.y = -20
+            self.on_ground = False
+            self.is_jumping = True
---
+++ movementTest.py
@@ -0,0 +1,69 @@
+import pygame
+from resources.sprite.spritesheet import Spritesheet
+from player import Player
+
+############# LOAD UP BASIC WINDOW AND CLOCK #############
+pygame.init()
+DISPLAY_W, DISPLAY_H = 1066, 600
+canvas = pygame.Surface((DISPLAY_W, DISPLAY_H))
+window = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
+running = True
+clock = pygame.time.Clock()
+TARGET_FPS = 60
+
+############# LOAD PLAYER AND SPRITESHEET #############
+my_spritesheet = Spritesheet("resources/sprite/azuball_spritesheet.png")
+player = Player()
+player.position.x, player.position.y = 100, DISPLAY_H
+
+############# MAIN GAME LOOP #############
+while running:
+    dt = clock.tick(60) * 0.001 * TARGET_FPS
+    ############# CHECK PLAYER INPUT #############
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.LEFT_KEY = True
+            elif event.key == pygame.K_RIGHT:
+                player.RIGHT_KEY = True
+            elif event.key == pygame.K_UP:
+                player.jump()
+        
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.LEFT_KEY = False
+            elif event.key == pygame.K_RIGHT:
+                player.RIGHT_KEY = False
+            elif event.key == pygame.K_UP:
+                if player.is_jumping:
+                    player.velocity.y *= 0.5 
+                    player.is_jumping = False
+    
+    
+    ############# UPDATE PLAYER #############
+    player.update(dt)
+    
+    ############# UPDATE WINDOW AND DISPLAY #############
+    canvas.fill((255, 255, 255))
+    player.draw(canvas)
+
+    ############# DISPLAY PLAYER INFO #############
+    font = pygame.font.Font(None, 36)
+    pos = f"Position: {player.position}"
+    pos_info = font.render(pos, 1, (0, 0, 0))
+    canvas.blit(pos_info, (0, 0))
+    
+    vel = f"Velocity: {player.velocity}"
+    vel_info = font.render(vel, 1, (0, 0, 0))
+    canvas.blit(vel_info, (0, 30))
+
+    acc = f"Acceleration: {player.acceleration}"
+    acc_info = font.render(acc, 1, (0, 0, 0))
+    canvas.blit(acc_info, (0, 60))
+    
+    window.blit(canvas, (0, 0))
+    pygame.display.update()
