3ddfbc8499c29a045fe978e5fd8850a417ca2655
cris <c.cariqueo03@ufromail.cl>
2025-03-15 17:53:14 -0300

    Added timer
    azuball.py
    + Display size turned into attribute
    + Added font attribute
    + Added time counter
    + Timer rendered
    + Added draw_timer()
    + Added secs_to_mins()
    + Added some comments to organize functions

--- azuball.py
+++ azuball.py
@@ -5,34 +5,41 @@ from ball import Ball
 
 class Azuball:
     def __init__(self):
-        ############# LOAD UP BASIC WINDOW AND CLOCK #############
+        ############# LOAD UP BASIC WINDOW AND VARIABLES #############
         pygame.init()
-        DISPLAY_W, DISPLAY_H = 1066, 600
-        self.canvas = pygame.Surface((DISPLAY_W, DISPLAY_H))
-        self.window = pygame.display.set_mode((DISPLAY_W, DISPLAY_H))
+        self.DISPLAY_W, self.DISPLAY_H = 1066, 600
+        self.canvas = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
+        self.window = pygame.display.set_mode((self.DISPLAY_W, self.DISPLAY_H))
         self.running = True
         self.clock = pygame.time.Clock()
         self.TARGET_FPS = 60
-        self.round_started = False
         self.show_throw_box = False
+        self.font = pygame.font.SysFont("comicsans", 36)
+
+        ############# GAME VARIABLES #############
+        self.round_started = False
+        self.round_over = False
+        self.round_winner = None
+        self.round_counter = 1
+        self.round_timer = 0
 
         ############# LOAD SPRITESHEET #############
         self.spritesheet = Spritesheet("resources/sprite/azuball_spritesheet.png")
 
         ############# LOAD PLAYER #############
         self.player_1 = Player("osaka_0.png", (pygame.K_a, pygame.K_d, pygame.K_w, pygame.K_e), (128, 274))
-        self.player_1.position.x, self.player_1.position.y = 130, DISPLAY_H
-        self.player_1.r_wall = DISPLAY_W/2
+        self.player_1.position.x, self.player_1.position.y = 130, self.DISPLAY_H
+        self.player_1.r_wall = self.DISPLAY_W/2
         self.player_1.throw_pos_offset.xy = 105, -5
         self.player_1.animator.create_animation(self.spritesheet, "osaka", "prep", .3, 4)
         self.player_1.animator.create_animation(self.spritesheet, "osaka_ball", "throw", .4)
         
         self.player_2 = Player("chiyo_0.png", (pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_MINUS), (128, 274))
         self.player_2.ID = True
-        self.player_2.position.x, self.player_2.position.y = DISPLAY_W-258, DISPLAY_H
+        self.player_2.position.x, self.player_2.position.y = self.DISPLAY_W-258, self.DISPLAY_H
         self.player_2.collide_rect_offset += 7
         self.player_2.collide_rect.height = self.player_2.rect.height * .8
-        self.player_2.l_wall = DISPLAY_W/2
+        self.player_2.l_wall = self.DISPLAY_W/2
         self.player_2.throw_pos_offset.xy = 20, 50
         self.player_2.animator.create_animation(self.spritesheet, "chiyo", "prep", .3, 4)
         self.player_2.animator.create_animation(self.spritesheet, "chiyo_ball", "throw", .4)
@@ -45,18 +52,22 @@ class Azuball:
         ############# LOAD VOLLEY ELEMENTS #############
         net_img = self.spritesheet.parse_sprite("net.png")  # 10x128
         net_img = pygame.transform.scale(net_img, (28, 358))
-        net_x, net_y = ((((DISPLAY_W-260)/2) - net_img.get_width()/2) - 5, DISPLAY_H-net_img.get_height()+10)
+        net_x, net_y = ((((self.DISPLAY_W-260)/2) - net_img.get_width()/2) - 5, self.DISPLAY_H-net_img.get_height()+10)
         self.background.blit(net_img, (net_x, net_y))
         self.net_rect = pygame.Rect((net_x + 135, net_y + 3), (net_img.get_width() - 3, 100))
         
         self.ball = Ball()
-        self.ball.position.x, self.ball.position.y = DISPLAY_W/2, 200
+        self.ball.position.x, self.ball.position.y = self.DISPLAY_W/2, 200
         self.ball.acceleration.y = 0
 
     def play(self):
         ############# MAIN GAME LOOP #############
         while self.running:
             dt = self.clock.tick(60) * 0.001 * self.TARGET_FPS
+            ############# ROUND TIMER #############
+            if self.round_started:
+                self.round_timer += dt/60
+            
             ############# CHECK PLAYER INPUT #############
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -93,6 +104,7 @@ class Azuball:
             self.player_1.draw(self.canvas)
             self.player_2.draw(self.canvas)
             self.ball.draw(self.canvas)
+            self.draw_timer()
 
             # Debug info, comment/uncomment as needed
             # self.show_rects()
@@ -105,6 +117,7 @@ class Azuball:
             self.window.blit(self.canvas, (0, 0))
             pygame.display.update()
     
+    # GAME LOGIC
     def check_player_keys(self, player: Player, event: pygame.event.Event):
         if event.type == pygame.KEYDOWN:
             if event.key == player.LEFT_KEY:
@@ -144,6 +157,25 @@ class Azuball:
             if not player.in_animation and not player.sel_animation:
                 self.throw_ball(player)
 
+    ## UI
+    def draw_timer(self):
+        timer_1 = self.font.render("time", 1, (0, 0, 0))
+        self.canvas.blit(timer_1, (self.DISPLAY_W/2 - timer_1.get_width()/2, 0))
+
+        timer_2 = self.font.render(":", 1, (0, 0, 0))
+        self.canvas.blit(timer_2, (self.DISPLAY_W/2 - timer_2.get_width()/2, 36))
+        
+        time = self.secs_to_mins(int(self.round_timer))
+        timer_3_info = f"{time[0]}:{time[1]}"
+        timer_3 = self.font.render(timer_3_info, 1, (0, 0, 0))
+        self.canvas.blit(timer_3, (self.DISPLAY_W/2 - timer_3.get_width()/2, 72))
+    
+    def secs_to_mins(self, secs: int):
+        mins = secs // 60
+        secs = secs % 60
+        return mins, secs
+
+    # TESTING
     def test_ball_physics(self, event: pygame.event.Event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
